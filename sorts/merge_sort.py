41bf982919ee36f1b8c9ab992250fc7926afe79e
Cédric Ernewein <25933633+Cernewein@users.noreply.github.com>
2023-04-28 12:25:16 +0200

    Added inversions count to merge sort algorithm.

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -32,3 +32,49 @@ def merge_sort(input_list: list[Union[float, int]]) -> list[Union[float, int]]:
         sorted_input_list_1 = merge_sort(input_list_1)
         sorted_input_list_2 = merge_sort(input_list_2)
         return merge(sorted_input_list_1, sorted_input_list_2)
+
+
+def merge_and_count_split_inversions(
+    input_list_1: list[Union[float, int]], input_list_2: list[Union[float, int]]
+) -> tuple[list[Union[float, int]], int]:
+    """Merges two previously sorted lists into a single sorted list and also returns the number of split inversions found.
+    The return value is a tuple (sorted_list, number_split_inversions)."""
+    merged_list = []
+    split_inversions = 0
+    while len(input_list_1) > 0 and len(input_list_2) > 0:
+        if input_list_1[0] <= input_list_2[0]:
+            merged_list.append(input_list_1[0])
+            input_list_1.pop(0)
+        else:
+            merged_list.append(input_list_2[0])
+            input_list_2.pop(0)
+            split_inversions += len(input_list_1)
+    if len(input_list_1) > 0:
+        merged_list += input_list_1
+    else:
+        merged_list += input_list_2
+    return merged_list, split_inversions
+
+
+def merge_sort_and_count_split_inversions(
+    input_list: list[Union[float, int]]
+) -> tuple[list[Union[float, int]], int]:
+    """Takes a list of numbers as input and returns a sorted list as well as the number of inversions in that list as output.
+    The list is sorted based on the MergeSort algorithm."""
+    list_length = len(input_list)
+    if list_length <= 1:
+        return input_list, 0
+    else:
+        input_list_1 = input_list[0 : list_length // 2]
+        input_list_2 = input_list[list_length // 2 :]
+        sorted_input_list_1, left_inversions = merge_sort_and_count_split_inversions(
+            input_list_1
+        )
+        sorted_input_list_2, right_inversions = merge_sort_and_count_split_inversions(
+            input_list_2
+        )
+        sorted_list, split_inversions = merge_and_count_split_inversions(
+            sorted_input_list_1, sorted_input_list_2
+        )
+        inversions = split_inversions + left_inversions + right_inversions
+        return sorted_list, inversions
--- tests/test_sorts/test_merge_sort.py
+++ tests/test_sorts/test_merge_sort.py
@@ -1,4 +1,9 @@
-from sorts.merge_sort import merge_sort, merge
+from sorts.merge_sort import (
+    merge_sort,
+    merge,
+    merge_sort_and_count_split_inversions,
+    merge_and_count_split_inversions,
+)
 
 
 def test_merge_sort():
@@ -10,3 +15,20 @@ def test_merge():
     input_list_1 = [1, 3, 5, 6, 7, 10]
     input_list_2 = [2, 4, 8, 9]
     assert merge(input_list_1, input_list_2) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_merge_sort_and_count_inversion():
+    input_list = [207, 101.3, 304, 12.3476, 98.0]
+    sorted_list, number_inversion = merge_sort_and_count_split_inversions(input_list)
+    assert sorted_list == [12.3476, 98.0, 101.3, 207, 304]
+    assert number_inversion == 3 + 2 + 2
+
+
+def test_merge_and_count_inversion():
+    input_list_1 = [1, 3, 5, 6, 7, 10]
+    input_list_2 = [2, 4, 8, 9]
+    merged_list, number_inversion = merge_and_count_split_inversions(
+        input_list_1, input_list_2
+    )
+    assert merged_list == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert number_inversion == 5 + 4 + 1 + 1
