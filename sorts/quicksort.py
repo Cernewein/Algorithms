c9dad9b9cdb150ffbf8ac9054b308998b4d76547
Cédric Ernewein <25933633+Cernewein@users.noreply.github.com>
2023-05-12 12:50:07 +0200

    upd: quicksort / Implemented three new pivot selection methods on top of the random pivot selection.

--- sorts/quicksort.py
+++ sorts/quicksort.py
@@ -1,8 +1,63 @@
 from typing import Union
 import random
+import statistics
 
 
-def partition(array: list[Union[int, float]], left: int, right: int) -> int:
+class ArgumentError(Exception):
+    pass
+
+
+def choose_pivot(
+    array: list[Union[int, float]], left: int, right: int, method: str
+) -> int:
+    """Chooses a pivot element for quicksort based on specified method.
+    Allowed methods are :
+        * 'left' : the leftmost element is chosen as pivot.
+        * 'right' : the rightmost element is chosen as pivot.
+        * 'random' : a random element is chosen as pivot
+        * 'median' : the median value of the three elements [left, right, middle] is chosen as pivot.
+
+    Args:
+        array : The array for which a pivot element should be chosen.
+        left : The left index for the subarray the method should be working on.
+        right  : The right index for the subarray the method should be working on.
+        method : The method to be used for pivot selection. Can be one of the four ['left', 'right', 'median', 'random'].
+
+    Raises:
+        ArgumentError: Raises an argument error when the selected method is not in the allowed list.
+
+    Returns:
+        The chosen pivot index
+    """
+    if method == "random":
+        pivot_index = random.randint(left, right)
+    elif method == "left":
+        pivot_index = left
+    elif method == "right":
+        pivot_index = right
+    elif method == "median":
+        values = []
+        values.append(array[left])
+        values.append(array[right])
+        middle_index = (left - right) // 2
+        values.append(array[middle_index])
+        median = statistics.median(values)
+        if array[left] == median:
+            pivot_index = left
+        elif array[right] == median:
+            pivot_index = right
+        else:
+            pivot_index = middle_index
+    else:
+        raise ArgumentError(
+            f"{method} method not recognized. Please provide one of the three methods ['left', 'right', 'median', 'random']."
+        )
+    return pivot_index
+
+
+def partition(
+    array: list[Union[int, float]], left: int, right: int, method: str
+) -> int:
     """Partition a subset of an array around a random pivot element.
     Elements smaller than the pivot will be placed on the left of the pivot.
     Elements greater than the pivot will be placed on the right of the pivot.
@@ -12,12 +67,13 @@ def partition(array: list[Union[int, float]], left: int, right: int) -> int:
         array : The input array to be partitioned
         left : The left index of the array subset
         right : The right index of the array subset
+        method : The method to be used for pivot selection. Can be one of the four ['left', 'right', 'median', 'random'].
 
     Returns:
         The pivot used for partitioning index
     """
     # Choose a random pivot element and place it at the beginning of the array
-    pivot_index = random.randint(left, right)
+    pivot_index = choose_pivot(array, left, right, method)
     (array[pivot_index], array[left]) = (array[left], array[pivot_index])
     pivot = array[left]
 
@@ -43,6 +99,7 @@ def quicksort(
     array: list[Union[int, float]],
     left: Union[int, None] = None,
     right: Union[int, None] = None,
+    method: str = "random",
 ):
     """Sort an array inplace. Uses the randomized quicksort algorithm in order to sort the array.
 
@@ -50,6 +107,7 @@ def quicksort(
         array : The input array that should be sorted
         left (optional): The left index of the subarray that should be sorted. Defaults to None and mostly used for recursion.
         right (optional): The right index of the subarray that sould be sorted. Defaults to None and mostly used for recursion.
+        method (optional): The method to be used for pivot selection. Can be one of the four ['left', 'right', 'median', 'random'].
     """
     if left is None:
         left = 0
@@ -59,10 +117,10 @@ def quicksort(
         # Choose a pivot element and partition the array such that
         # elements smaller than pivot are on the left
         # elements greater than pivot are on the right
-        pivot = partition(array, left, right)
+        pivot = partition(array, left, right, method)
 
         # Recursive call on the left of pivot
-        quicksort(array, left, pivot - 1)
+        quicksort(array, left, pivot - 1, method)
 
         # Recursive call on the right of pivot
-        quicksort(array, pivot + 1, right)
+        quicksort(array, pivot + 1, right, method)
--- tests/test_sorts/test_quicksort.py
+++ tests/test_sorts/test_quicksort.py
@@ -2,9 +2,34 @@ from sorts.quicksort import quicksort, partition
 
 
 def test_quicksort():
+    # Testing the quicksort with random pivot choice
     input_list = [2, 3, 1, 5]
-    quicksort(input_list)
+    quicksort(input_list, method="random")
     assert input_list == [1, 2, 3, 5]
     input_list = [207, 101.3, 304, 12.3476, 98.0]
-    quicksort(input_list)
+    quicksort(input_list, method="random")
+    assert input_list == [12.3476, 98.0, 101.3, 207, 304]
+
+    # Testing the quicksort with median method pivot choice
+    input_list = [2, 3, 1, 5]
+    quicksort(input_list, method="median")
+    assert input_list == [1, 2, 3, 5]
+    input_list = [207, 101.3, 304, 12.3476, 98.0]
+    quicksort(input_list, method="median")
+    assert input_list == [12.3476, 98.0, 101.3, 207, 304]
+
+    # Testing the quicksort with left element chosen as pivot
+    input_list = [2, 3, 1, 5]
+    quicksort(input_list, method="left")
+    assert input_list == [1, 2, 3, 5]
+    input_list = [207, 101.3, 304, 12.3476, 98.0]
+    quicksort(input_list, method="left")
+    assert input_list == [12.3476, 98.0, 101.3, 207, 304]
+
+    # Testing the quicksort with right element chosen as pivot
+    input_list = [2, 3, 1, 5]
+    quicksort(input_list, method="right")
+    assert input_list == [1, 2, 3, 5]
+    input_list = [207, 101.3, 304, 12.3476, 98.0]
+    quicksort(input_list, method="right")
     assert input_list == [12.3476, 98.0, 101.3, 207, 304]
