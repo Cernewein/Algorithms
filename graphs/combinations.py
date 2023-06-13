b17c8efdd26cbe157627254a58140de1552c4841
Cédric Ernewein <25933633+Cernewein@users.noreply.github.com>
2023-06-13 15:04:34 +0200

    add : graphs / Added function for generating combinations of elements with replacement given a list of elements and the number of elements to be combined

--- graphs/combinations.py
+++ graphs/combinations.py
@@ -12,19 +12,22 @@ def combinations(
         collection: The input list of elements to be combined
         number_elements : The number of elements in a combination
 
+    Raises:
+        ArgumentError: In case of illegal number_elements argument
+
     Returns:
         Returns a list containing all possible combinations of number_elements made up of elements in collection
     """
-    if number_elements == 0:
-        return [collection]
-    if number_elements == 1:
-        return [[element] for element in collection]
-
     if number_elements < 0:
         raise ArgumentError("Number of element cannot be negative.")
 
     if number_elements % 1 != 0:
         raise ArgumentError("Number of elements cannot be a float.")
+
+    if number_elements == 0:
+        return [collection]
+    if number_elements == 1:
+        return [[element] for element in collection]
 
     if (number_elements > len(collection)) & (not recursion):
         raise ArgumentError(
@@ -36,6 +39,44 @@ def combinations(
         subsets = [
             sub
             for sub in combinations(collection[(n + 1) :], number_elements - 1, True)
+        ]
+        for subset in subsets:
+            result.append([element] + subset)
+    return result
+
+
+def combinations_with_replacement(collection: list, number_elements: int):
+    """Generate all combinations WITH replacement given a collection of elements and the desired number of elements to be combined.
+    Uses DFS to generate all combinations.
+
+    Args:
+        collection: The input list of elements to be combined
+        number_elements : The number of elements in a combination
+
+    Raises:
+        ArgumentError: In case of illegal number_elements argument
+
+    Returns:
+        Returns a list containing all possible combinations WITH replacement of number_elements made up of elements in collection
+    """
+    if number_elements < 0:
+        raise ArgumentError("Number of element cannot be negative.")
+
+    if number_elements % 1 != 0:
+        raise ArgumentError("Number of elements cannot be a float.")
+
+    if number_elements == 0:
+        return [collection]
+    if number_elements == 1:
+        return [[element] for element in collection]
+
+    result = []
+    for n, element in enumerate(collection):
+        subsets = [
+            sub
+            for sub in combinations_with_replacement(
+                collection[n:], number_elements - 1
+            )
         ]
         for subset in subsets:
             result.append([element] + subset)
--- tests/test_graphs/test_combinations.py
+++ tests/test_graphs/test_combinations.py
@@ -1,4 +1,4 @@
-from graphs.combinations import combinations
+from graphs.combinations import combinations, combinations_with_replacement
 import pytest
 from errors.errors import ArgumentError
 
@@ -34,6 +34,59 @@ def test_combinations():
         match=f"Number of elements {number_elements} exceeds the length of the list {len(l)}",
     ):
         combinations(l, number_elements)
+
+    # Number of elements negative
+    number_elements = -5
+    l = [1, 2, 3, 4]
+
+    with pytest.raises(
+        ArgumentError,
+        match="Number of element cannot be negative.",
+    ):
+        combinations(l, number_elements)
+
+    # Number of elements cannot be a float
+    number_elements = 3.2
+    l = [1, 2, 3, 4]
+
+    with pytest.raises(ArgumentError, match="Number of elements cannot be a float."):
+        combinations(l, number_elements)
+
+
+def test_combinations_with_replacement():
+    # Testing the base case
+    number_elements = 1
+    l = [1, 2, 3, 4]
+
+    assert [[1], [2], [3], [4]] == combinations_with_replacement(l, number_elements)
+
+    # Testing with more elements to combine
+    number_elements = 2
+    l = [1, 2, 3, 4]
+
+    assert [
+        [1, 1],
+        [1, 2],
+        [1, 3],
+        [1, 4],
+        [2, 2],
+        [2, 3],
+        [2, 4],
+        [3, 3],
+        [3, 4],
+        [4, 4],
+    ] == combinations_with_replacement(l, number_elements)
+
+    # Number of elements greater than the length of the list
+    number_elements = 3
+    l = [1, 2]
+
+    assert [
+        [1, 1, 1],
+        [1, 1, 2],
+        [1, 2, 2],
+        [2, 2, 2],
+    ] == combinations_with_replacement(l, number_elements)
 
     # Number of elements negative
     number_elements = -5
