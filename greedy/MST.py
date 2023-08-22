1ef5b55d810deebe56ceb90ad726633a9e83db3e
Cédric Ernewein <25933633+Cernewein@users.noreply.github.com>
2023-08-22 08:55:17 +0200

    add : Minimum Spanning Tree / Added a version of Prim's algorithm making use of the heap data structure

--- greedy/MST.py
+++ greedy/MST.py
@@ -1,4 +1,4 @@
-from graphs.datastructures import UndirectedGraph
+from graphs.datastructures import UndirectedGraph, MinHeap
 import random
 
 
@@ -11,10 +11,14 @@ def prim(input_graph: UndirectedGraph) -> UndirectedGraph:
     Returns:
         UndirectedGraph: The minimum spanning tree.
     """
+    if type(input_graph) != UndirectedGraph:
+        raise TypeError("Please provide an input graph of type UndirectedGraph")
+    # Initializing the minimum spanning tree with a randomly chosen node
     input_graph_nodes = input_graph.get_nodes()
     number_nodes = len(input_graph_nodes)
     starting_node = input_graph_nodes[random.randint(0, number_nodes - 1)]
     nodes_in_mst = set([starting_node])
+    # Initializing accessible edges
     accessible_edges = [
         [starting_node, adjacent_node]
         for adjacent_node in list(input_graph.get_adjacent_nodes(starting_node))
@@ -25,7 +29,7 @@ def prim(input_graph: UndirectedGraph) -> UndirectedGraph:
         # Initiliazing the shortest distance and the closest node
         min_distance = 1e7
         closest_node = accessible_edges[0][1]
-        # Iterating over all accessible edges to find the next node to be chosen : the one that has the current minimum path
+        # Iterating over all accessible edges to find the next node to be chosen : the one that has the current minimum distance
         for edge in accessible_edges:
             start_node = edge[0]
             adjacent_node = edge[1]
@@ -34,6 +38,7 @@ def prim(input_graph: UndirectedGraph) -> UndirectedGraph:
                 min_distance = distance
                 starting_node_to_closest = start_node
                 closest_node = adjacent_node
+        # Once the closest node has been selected it can be added to the minimum spanning tree
         nodes_in_mst.add(closest_node)
         minimum_spanning_tree.add_node(closest_node)
         minimum_spanning_tree.add_edge(
@@ -45,6 +50,53 @@ def prim(input_graph: UndirectedGraph) -> UndirectedGraph:
             for adjacent_node in input_graph.get_adjacent_nodes(start_node):
                 if adjacent_node not in nodes_in_mst:
                     accessible_edges.append([start_node, adjacent_node])
+    return minimum_spanning_tree
+
+
+def prim_heap(input_graph: UndirectedGraph) -> UndirectedGraph:
+    """Compute a minimum spanning tree thanks to Prim's algorithm and leveraging the heap data structure for minimum computational overhead.
+
+    Args:
+        input_graph (UndirectedGraph): The input graph for which a minimum spanning tree should be computed
+
+    Returns:
+        UndirectedGraph: The minimum spanning tree.
+    """
+    if type(input_graph) != UndirectedGraph:
+        raise TypeError("Please provide an input graph of type UndirectedGraph")
+    # Initializing the minimum spanning tree with a randomly chosen node
+    input_graph_nodes = input_graph.get_nodes()
+    number_nodes = len(input_graph_nodes)
+    starting_node = input_graph_nodes[random.randint(0, number_nodes - 1)]
+    nodes_in_mst = set([starting_node])
+    minimum_spanning_tree = UndirectedGraph(weighted=True)
+    minimum_spanning_tree.add_node(starting_node)
+
+    # Initializing accessible edges. These are stored in a heap so that the min value can be kept track of.
+    accessible_edges = MinHeap()
+    for adjacent_node, weight in input_graph.get_adjacent_nodes_and_weights(
+        starting_node
+    ):
+        # The edges are always stored as tuples (starting_node, ending_node)
+        accessible_edges.insert((starting_node, adjacent_node), weight)
+
+    # While there are edges going from explored to unexplored edges there are paths that exist
+    while accessible_edges.get_number_nodes() != 0:
+        # Extracting the node with the shortest path stored in the heap
+        edge, distance = accessible_edges.extract_min()
+        # If the adjacent edge is already in the minimum spanning tree it doesn't need to be added.
+        if edge[1] in nodes_in_mst:
+            continue
+        nodes_in_mst.add(edge[1])
+        minimum_spanning_tree.add_node(edge[1])
+        minimum_spanning_tree.add_edge(edge[0], edge[1], distance)
+        # Updating the heap with the new distances after having explored a new node.
+        for adjacent_node, weight in input_graph.get_adjacent_nodes_and_weights(
+            edge[1]
+        ):
+            if adjacent_node not in nodes_in_mst:
+                accessible_edges.insert((edge[1], adjacent_node), weight)
+
     return minimum_spanning_tree
 
 
--- tests/test_greedy/test_mst.py
+++ tests/test_greedy/test_mst.py
@@ -1,4 +1,4 @@
-from greedy.MST import prim
+from greedy.MST import prim, prim_heap
 from graphs.datastructures import UndirectedGraph
 
 
@@ -16,3 +16,19 @@ def test_prim(test_graph_weighted_2):
     miniminum_spanning_tree.add_edge(3, 5, 4)
 
     assert miniminum_spanning_tree == prim(test_graph_weighted_2)
+
+
+def test_prim_heap(test_graph_weighted_2):
+    miniminum_spanning_tree = UndirectedGraph(weighted=True)
+    miniminum_spanning_tree.add_node(1)
+    miniminum_spanning_tree.add_node(2)
+    miniminum_spanning_tree.add_node(3)
+    miniminum_spanning_tree.add_node(4)
+    miniminum_spanning_tree.add_node(5)
+
+    miniminum_spanning_tree.add_edge(1, 2, 2)
+    miniminum_spanning_tree.add_edge(2, 4, 3)
+    miniminum_spanning_tree.add_edge(4, 3, 2)
+    miniminum_spanning_tree.add_edge(3, 5, 4)
+
+    assert miniminum_spanning_tree == prim_heap(test_graph_weighted_2)
