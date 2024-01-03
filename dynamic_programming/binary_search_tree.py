bfd3ed509ab8b74134b10360738872e83c34c764
Cédric Ernewein <25933633+Cernewein@users.noreply.github.com>
2024-01-03 18:13:11 +0100

    add : dynamic programming / optBST / Added an implementation for the search time of the optimal binary search tree built on top of optBST but making use of Knuth's optimization on top.

--- dynamic_programming/binary_search_tree.py
+++ dynamic_programming/binary_search_tree.py
@@ -40,5 +40,59 @@ def optBST(keys: list[str|int|float], frequencies: list[int|float]) -> float:
                 solution_array[i][i + problem_size] = sum(frequencies[(i):(i + problem_size + 1)]) + min_subtree_search_time
             else:
                 solution_array[i][i + problem_size] = sum(frequencies[(i):(i + problem_size + 1)])
+    return solution_array[0][num_elements-1]
+
+def optBSTKnuth(keys: list[str|int|float], frequencies: list[int|float]) -> float:
+    """This function builds upon optBST but adds Knuth's optimization for reducing the running time from O(n^3) to O(n^2).
+    The main idea is that not all possible roots need to be inspected, by only those situated between the two subtree roots 
+    (as otherwise the elements are already contained in on of the two subtrees, there is therefore no need to inspect them).
+
+    Args:
+        keys (list[str | int | float]): The elements to order
+        frequencies (list[int | float]): The loockup frequencies associated to each element
+
+    Raises:
+        IndexError
+
+    Returns:
+        float: The minimum search time associated to the optimal binary search tree.
+    """
+    num_elements = len(keys)
+    if num_elements != len(frequencies):
+        raise IndexError("Please provide two arrays of the same length.")
+    
+    # Initialize the solution array
+    solution_array = [[0 for i in range(num_elements + 1)] for j in range(num_elements + 1)]
+    subtree_roots = {}
+
+
+    # Fill out the solution array starting from the diagonal towards the north-west
+    for problem_size in range(0, num_elements):
+        for i in range(0, num_elements - problem_size):
+            min_subtree_search_time = 1000000000
+            has_found_min = False
+            start_root = subtree_roots.get((i, i + problem_size - 1))
+            end_root = subtree_roots.get((i + 1, i + problem_size))
+            if start_root == None:
+                start_root = i
+            if end_root == None:
+                end_root = problem_size + i
+            for r in range(start_root, end_root + 1):
+                # Looking for the root generating the smallest overall search time stemming from two subtrees associated to the selected root.
+                try:
+                    subtree_search_time = solution_array[i][r - 1] + solution_array[r + 1][i + problem_size]
+                except IndexError:
+                    # Handling the case when r + i is greater than the number of elements. ie. when the inspected root would only have a left subtree
+                    # This special case
+                    subtree_search_time = solution_array[i][r - 1]
+                if min_subtree_search_time > subtree_search_time:
+                    min_subtree_search_time = subtree_search_time
+                    subtree_roots[(i,i + problem_size)] = r
+                    has_found_min = True
+            if has_found_min:
+                solution_array[i][i + problem_size] = sum(frequencies[(i):(i + problem_size + 1)]) + min_subtree_search_time
+            else:
+                solution_array[i][i + problem_size] = sum(frequencies[(i):(i + problem_size + 1)])
+
     print(solution_array)
     return solution_array[0][num_elements-1]
--- tests/test_dynamic_programming/test_binary_search_tree.py
+++ tests/test_dynamic_programming/test_binary_search_tree.py
@@ -18,3 +18,21 @@ def test_optBST_value():
 
     assert 1.3 == optBST(keys, frequencies)
 
+
+def test_optBSTKnuth_value():
+    # Case when all frequencies are equal
+    # Here the optimal tree is a balanced one
+    #    1
+    #   / \
+    #  2   3
+    keys = [1, 2, 3]
+    frequencies = [1, 1, 1]
+
+    assert 5 == optBSTKnuth(keys, frequencies)
+
+    # Case when frequencies are different from one another
+    # Here the otimal tree is a linear tree 1 -> 2 -> 3
+    keys = [1, 2, 3]
+    frequencies = [0.8, 0.1, 0.1]
+
+    assert 1.3 == optBSTKnuth(keys, frequencies)
