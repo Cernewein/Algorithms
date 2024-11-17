f515485db85a5236db070a445ccc76af158c687f
Cédric Ernewein <25933633+Cernewein@users.noreply.github.com>
2024-11-17 16:21:13 +0100

    rm : two_opt_tsp

--- graphs/two_opt_tsp.py
+++
@@ -1,6 +0,0 @@
-from graphs.datastructures import UndirectedGraph
-from greedy.nearest_neighbor_tsp import nearest_neighbor_tsp
-
-def two_opt_tsp(locations_graph:UndirectedGraph, starting_node:object) -> UndirectedGraph:
-    # Creating an initial tour
-    tsp_tour = nearest_neighbor_tsp(locations_graph, starting_node)
